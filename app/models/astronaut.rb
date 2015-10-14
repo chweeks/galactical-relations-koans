99cdadb4b9275774f85f4986700a087c21b6dc90
chweeks <chrisharryweeks@gmail.com>
2015-10-14 23:19:48 +0100

    completed stage delta

--- app/models/astronaut.rb
+++ app/models/astronaut.rb
@@ -1,2 +1,9 @@
 class Astronaut < ActiveRecord::Base
+
+  has_many :crewings
+  has_many :spaceships, through: :crewings
+
+  def spaceship
+    crewings.where(captain: true).first.spaceship
+  end
 end
--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,2 +1,9 @@
 class Spaceship < ActiveRecord::Base
+
+  has_many :crewings
+  has_many :astronauts, through: :crewings
+
+  def captain
+    crewings.where(captain: true).first.astronaut
+  end
 end
